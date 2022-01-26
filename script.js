989c6b59854a3c93440b965154d6070c8b8793ec
Giulia Polenta <giuliapolenta@gmail.com>
2022-01-26 14:26:42 +0100

    add homepage variables for review

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// window.onload = functions;
+
+// function functions() {
+//     textColors();
+// }
+
+// function textColors() {
+//     const text = document.querySelectorAll(".copy p");
+//     const container = document.querySelector(".main-content");
+//     const copyLinks = document.querySelectorAll(".copy a");
+
+//     text.forEach(element => {
+//         element.addEventListener("mouseenter", () => {
+//             container.classList.add("blend");
+//             copyLinks.forEach(link => {
+//                 link.style.color = "#1c1cff";
+//             });
+//         });
+//         element.addEventListener("mouseleave", () => {
+//             container.classList.remove("blend");
+//             copyLinks.forEach(link => {
+//                 link.style.color = "#feb398";
+//             });
+//         });
+//     });
+// }
+
 window.onload = functions;
 
 function functions() {
@@ -5,22 +32,13 @@ function functions() {
 }
 
 function textColors() {
-    const text = document.querySelectorAll(".copy p");
-    const container = document.querySelector(".main-content");
-    const copyLinks = document.querySelectorAll(".copy a");
-
-    text.forEach(element => {
-        element.addEventListener("mouseenter", () => {
-            container.classList.add("blend");
-            copyLinks.forEach(link => {
-                link.style.color = "#1c1cff";
-            });
+    let copyLinks = document.querySelectorAll(".copy a");
+    copyLinks.forEach(link => {
+        link.addEventListener("mouseenter", () => {
+            link.style.color = "#1c1cff";
         });
-        element.addEventListener("mouseleave", () => {
-            container.classList.remove("blend");
-            copyLinks.forEach(link => {
-                link.style.color = "#feb398";
-            });
+        link.addEventListener("mouseleave", () => {
+            link.style.color = "#feb398";
         });
     });
 }
